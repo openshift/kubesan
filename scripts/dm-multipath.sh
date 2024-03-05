b95e9eb44be9f14055af42bbe0513b4c966b1e59
Alberto Faria <afaria@redhat.com>
2024-03-05 23:03:35 +0000

    Don't recreate dm-multipath target upon blob pool migration
    Otherwise, queued I/O is lost. We instead must modifiy the paths of the
    dm-multipath target without replacing it with a new target.
    
    It isn't possible to add or remove arbitrary paths, only to enable and
    disable existing paths. We thus point dm-multipath at a "lower" dm
    volume whose table we can modify to make it point to different volumes
    using a dm-linear target, or none at all using a dm-error target.
    
    Signed-off-by: Alberto Faria <afaria@redhat.com>

--- scripts/dm-multipath.sh
+++ scripts/dm-multipath.sh
@@ -12,16 +12,18 @@ __run_ignoring_error() {
     output=$( "${@:2}" 2>&1 | tee /dev/stderr ) || grep -iE "$1" <<< "$output"
 }
 
-# Usage: __table <size> [<connect_to_path>]
-__table() {
-    # dm-thin is bio-based, so dm-multipath has to be bio-based as well, hence
-    # the "queue_mode bio" option.
-    echo -n "0 $(( $1 / 512 )) multipath 3 queue_if_no_path queue_mode bio 0"
+# Usage: __lower_table [<connect_to_path>]
+__lower_table() {
     if (( $# == 1 )); then
-        echo " 0 0"
+        echo "0 $(( vol_size / 512 )) linear $1 0"
     else
-        echo " 1 1 round-robin 0 1 0 $2"
+        echo "0 $(( vol_size / 512 )) error"
     fi
+}
+
+# Usage: __upper_table
+__upper_table() {
+    echo "0 $(( vol_size / 512 )) multipath 3 queue_if_no_path queue_mode bio 0 1 1 round-robin 0 1 0 /dev/mapper/$vol_name-lower"
 }
 
 case "${command}" in
@@ -29,29 +31,44 @@ case "${command}" in
         vol_size=$3
         connect_to_path=$4
 
-        # TODO: make idempotent
-        dmsetup create "$vol_name" --table "$( __table "$vol_size" "$connect_to_path" )"
+        __run_ignoring_error "device or resource busy" \
+            dmsetup create "$vol_name-lower" --table "$( __lower_table "$connect_to_path" )"
+
+        dmsetup mknodes "$vol_name-lower"
+
+        __run_ignoring_error "device or resource busy" \
+            dmsetup create "$vol_name" --table "$( __upper_table )"
         ;;
 
     remove)
         # --force replaces table with error target, which prevents hangs when
         # removing a disconnected volume
+
         __run_ignoring_error "no such device or address" \
             dmsetup remove "$vol_name" --force
+
+        __run_ignoring_error "no such device or address" \
+            dmsetup remove "$vol_name-lower" --force
         ;;
 
     connect)
         vol_size=$3
         connect_to_path=$4
 
-        dmsetup load "$vol_name" --table "$( __table "$vol_size" "$connect_to_path" )"
-        dmsetup resume "$vol_name"
+        dmsetup suspend "$vol_name-lower"  # flush any in-flight I/O
+        dmsetup load "$vol_name-lower" --table "$( __lower_table "$connect_to_path" )"
+        dmsetup resume "$vol_name-lower"
+
+        dmsetup message "$vol_name" 0 "reinstate_path /dev/mapper/$vol_name-lower"
         ;;
 
     disconnect)
         vol_size=$3
 
-        dmsetup load "$vol_name" --table "$( __table "$vol_size" )"
-        dmsetup resume "$vol_name" --noudevsync  # this hangs without --noudevsync
+        dmsetup message "$vol_name" 0 "fail_path /dev/mapper/$vol_name-lower"
+
+        dmsetup suspend "$vol_name-lower"  # flush any in-flight I/O
+        dmsetup load "$vol_name-lower" --table "$( __lower_table )"
+        dmsetup resume "$vol_name-lower" --noudevsync  # this hangs without --noudevsync
         ;;
 esac
