a0f1cdd61b751a4a1ddc8e27edb012566348aebc
Stefan Hajnoczi <stefanha@redhat.com>
2024-04-23 09:19:51 -0400

    Add LVM profile with autoextend configuration
    LVM profiles are lvm.conf configuration that can be applied to specific
    VGs, LVs, or command invocations. Profiles allow Subprovisioner to
    specify LVM configuration settings without interfering with the
    system-wide lvm.conf file.
    
    Set the autoextend threshold to 95% of capacity so that thin pools are
    automatically grown when the threshold is reached.  See lvmthin(7) for
    details.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- pkg/subprovisioner/util/lvm/lvm.go
+++ pkg/subprovisioner/util/lvm/lvm.go
@@ -44,3 +44,51 @@ func Command(ctx context.Context, command string, arg ...string) (string, error)
 
 	return string(stdout), err
 }
+
+func WriteProfile(name string, contents string) error {
+	// This should never happen but be extra careful since the name is used
+	// to build a path outside the container's mount namespace and
+	// container escapes must be prevented.
+	if strings.ContainsAny(name, "/") {
+		return status.Errorf(codes.Internal, "lvm profile name \"%s\" must not contain a '/' character", name)
+	}
+	if name == ".." {
+		return status.Errorf(codes.Internal, "lvm profile name \"%s\" must not be \"..\"", name)
+	}
+
+	// This process runs in the node's PID namespace, so the node's root
+	// directory is accessible through the init process.
+	dir := "/proc/1/root/etc/lvm/profile"
+	path := dir + "/" + name + ".profile"
+
+	f, err := os.CreateTemp(dir, "subprovisioner-*")
+	if err != nil {
+		return status.Errorf(codes.Internal, "failed to create temporary file for lvm profile \"%s\": %s", name, err)
+	}
+	fClosed := false
+	fRenamed := false
+	defer func() {
+		if !fClosed {
+			f.Close()
+		}
+		if !fRenamed {
+			os.Remove(f.Name())
+		}
+	}()
+
+	if err := f.Chmod(0644); err != nil {
+		return status.Errorf(codes.Internal, "failed to chmod lvm profile \"%s\": %s", name, err)
+	}
+	if _, err := f.WriteString(contents); err != nil {
+		return status.Errorf(codes.Internal, "failed to write lvm profile \"%s\": %s", name, err)
+	}
+	if err := f.Close(); err != nil {
+		return status.Errorf(codes.Internal, "failed to close lvm profile \"%s\": %s", name, err)
+	}
+	fClosed = true
+	if err := os.Rename(f.Name(), path); err != nil {
+		return status.Errorf(codes.Internal, "failed to rename lvm profile \"%s\": %s", name, err)
+	}
+	fRenamed = true
+	return nil
+}
