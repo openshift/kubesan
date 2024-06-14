2de4f4b15fb6a8ac4763598eef369dafc28535da
Eric Blake <eblake@redhat.com>
2024-06-14 17:15:05 -0500

    tests: start running csi-sanity
    The csi-sanity testsuite tests whether a CSI driver handles various
    low-level details correctly, such as correct error handling.  While we
    expect kubernetes to be a well-behaved client (and not tickle as many
    corner cases), it is also a good idea to be robust even to a malicious
    client that might intentionally pass bad parameters.
    
    This exposes lots of failures, so for now I'm marking the test as
    skipped, and followup patches will be needed to clean it up.  Once our
    blob CRDs are more well-defined, we can require the test to pass CI.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>

---
+++ tests/t/csi-sanity.sh
@@ -0,0 +1,51 @@
+# SPDX-License-Identifier: Apache-2.0
+
+kubectl create -f - <<EOF
+apiVersion: v1
+kind: Pod
+metadata:
+  name: csi-sanity
+spec:
+  restartPolicy: Never
+  hostPID: true
+  containers:
+    - name: container
+      image: localhost/kubesan/test:test
+      command:
+        - ./csi-sanity
+        - --csi.controllerendpoint
+        - /run/csi/kubesan-controller/socket
+        - --csi.endpoint
+        - /run/csi/kubesan-node/socket
+        - --csi.testvolumeaccesstype
+        - block
+        - --ginkgo.v
+        - --ginkgo.seed=1
+#        - --ginkgo.fail-fast
+      volumeMounts:
+        - name: drivers
+          mountPath: /run/csi
+      securityContext:
+        privileged: true
+  volumes:
+    - name: drivers
+      hostPath:
+        path: /var/lib/kubelet/plugins/
+        type: DirectoryOrCreate
+EOF
+
+fail=0
+ksan-wait-for-pod-to-succeed 60 csi-sanity || fail=$?
+kubectl logs pods/csi-sanity
+
+# TODO fix remaining issues, then hard-fail this test if csi-sanity fails.
+# For now, if we got at least one pass in the final line of the logged output,
+# then mark this overall test as skipped instead of failed.
+pattern="[1-9][0-9]* Pass"
+if [[ $fail != 0 &&
+      "$( kubectl logs --tail=1 pods/csi-sanity )" =~ $pattern ]]; then
+    fail=77
+    echo "SKIP: partial csi-sanity failures are still expected"
+fi
+
+exit $fail
