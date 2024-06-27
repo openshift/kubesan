openshift/kubesan

commit 61b29814d1d46f380d8ded34e4d8148399de1c63

Use the node's lvm and sanlock
Don't ship lvm and sanlock as part of the container image. They are not
designed to run inside containers and there should only be one instance
per machine.

From now on, the user is responsible for setting up lvm and sanlock on
every node before using Subprovisioner. The expectation is that
operators will automate this so that actual end users do not need to
manually set up these components.

It is necessary to give Subprovisioner the ability to execute the node's
lvm utilities. Add hostPID: true to the PodSpecs so that nsenter(1) can
be used to invoke lvm on the node. This reduces container isolation and
weakens security, but it may be possible to improve this in the future.

Use a new new minikube ISO that includes lvmlockd and sanlock in the
test suite and tweak it to set up lvm.conf, sanlock.conf, etc on test
nodes.

This is an invasive commit that changes many things at once, but it is
difficult to split this into bisectable incremental commits.

Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

scripts/lvm.sh
@@ -15,8 +15,14 @@ __run_ignoring_error() {
     output=$( "${@:2}" 2>&1 | tee /dev/stderr ) || grep -iE "$1" <<< "$output"
 }
 
+# Run LVM commands outside container because they are designed for a single
+# system-wide instance and do not support containers.
+__lvm() {
+    nsenter --target 1 --all lvm "$@"
+}
+
 __lockstart() {
-    lvm vgchange \
+    __lvm vgchange \
         --devices "$backing_device_path" \
         --lock-start \
         subprovisioner
@@ -37,7 +43,7 @@ case "${command}" in
         # create LVM thin *pool* LV
 
         __run_ignoring_error "already exists in volume group" \
-            lvm lvcreate \
+            __lvm lvcreate \
             --devices "$backing_device_path" \
             --activate n \
             --type thin-pool \
@@ -48,7 +54,7 @@ case "${command}" in
 	    # create LVM thin LV
 
         __run_ignoring_error "already exists in volume group" \
-            lvm lvcreate \
+            __lvm lvcreate \
             --devices "$backing_device_path" \
             --type thin \
             --name "$lvm_thin_lv_name" \
@@ -59,7 +65,7 @@ case "${command}" in
         # deactivate LVM thin LV (`--activate n` has no effect on `lvcreate
         # --type thin`)
 
-        lvm lvchange \
+        __lvm lvchange \
             --devices "$backing_device_path" \
             --activate n \
             "subprovisioner/$lvm_thin_lv_name"
@@ -72,7 +78,7 @@ case "${command}" in
         # create snapshot LVM thin LV
 
         __run_ignoring_error "already exists in volume group" \
-            lvm lvcreate \
+            __lvm lvcreate \
             --devices "$backing_device_path" \
             --name "$lvm_thin_lv_name" \
             --snapshot \
@@ -82,7 +88,7 @@ case "${command}" in
         # deactivate LVM thin LV (`--activate n` has no effect on `lvcreate
         # --type thin`)
 
-        lvm lvchange \
+        __lvm lvchange \
             --devices "$backing_device_path" \
             --activate n \
             "subprovisioner/$lvm_thin_lv_name"
@@ -94,14 +100,14 @@ case "${command}" in
 	    # remove LVM thin LV
 
         __run_ignoring_error "failed to find logical volume" \
-            lvm lvremove \
+            __lvm lvremove \
             --devices "$backing_device_path" \
             "subprovisioner/$lvm_thin_lv_name"
 
 	    # remove LVM thin *pool* LV if there are no more thin LVs
 
         __run_ignoring_error "failed to find logical volume|removing pool \S+ will remove" \
-            lvm lvremove \
+            __lvm lvremove \
             --devices "$backing_device_path" \
             "subprovisioner/$lvm_thin_pool_lv_name"
         ;;
@@ -109,7 +115,7 @@ case "${command}" in
     activate-pool)
         # activate LVM thin *pool* LV
 
-        lvm lvchange \
+        __lvm lvchange \
             --devices "$backing_device_path" \
             --activate ey \
             "subprovisioner/$lvm_thin_pool_lv_name"
@@ -118,7 +124,7 @@ case "${command}" in
     deactivate-pool)
         # deactivate LVM thin *pool* LV
 
-        lvm lvchange \
+        __lvm lvchange \
             --devices "$backing_device_path" \
             --activate n \
             "subprovisioner/$lvm_thin_pool_lv_name"
@@ -129,7 +135,7 @@ case "${command}" in
 
         # activate LVM thin LV
 
-        lvm lvchange \
+        __lvm lvchange \
             --devices "$backing_device_path" \
             --activate ey \
             "subprovisioner/$lvm_thin_lv_name"
@@ -140,7 +146,7 @@ case "${command}" in
 
         # deactivate LVM thin LV
 
-        lvm lvchange \
+        __lvm lvchange \
             --devices "$backing_device_path" \
             --activate n \
             "subprovisioner/$lvm_thin_lv_name"

pkg/subprovisioner/util/lvm/lvm.go
@@ -14,8 +14,8 @@ import (
 
 // This returns an error if ctx is canceled, but never attempts to kill the process before it terminates.
 func Command(ctx context.Context, command string, arg ...string) (string, error) {
-	fullArgs := append([]string{command}, arg...)
-	cmd := exec.Command("lvm", fullArgs...)
+	fullArgs := append([]string{"--target", "1", "--all", "lvm", command}, arg...)
+	cmd := exec.Command("nsenter", fullArgs...)
 
 	cmd.Env = os.Environ()
 	cmd.Env = append(cmd.Env, "DM_DISABLE_UDEV=")

Containerfile
@@ -16,14 +16,10 @@ RUN go build -o bin/subprovisioner ./cmd/subprovisioner
 # FROM quay.io/centos/centos:stream9
 FROM quay.io/fedora/fedora:39
 
-RUN dnf install -qy lvm2-lockd nbd nbdkit-basic-plugins sanlock && dnf clean all
+RUN dnf install -qy nbd nbdkit-basic-plugins && dnf clean all
 
 WORKDIR /subprovisioner
 
-# prevent LVM commands from failing due to thinking that lvmlockd isn't running
-RUN touch /run/lvmlockd.pid
-
-COPY conf/lvm.conf /etc/lvm/
 COPY scripts/ scripts/
 
 COPY --from=builder /subprovisioner/bin/subprovisioner ./

conf/lvm.conf
@@ -1,8 +1,0 @@
-global {
-	use_lvmlockd = 1
-}
-
-activation {
-	udev_sync = 0
-	udev_rules = 0
-}

deploy/01-csi-controller-plugin.yaml
@@ -24,6 +24,7 @@ spec:
       labels: *labels
     spec:
       serviceAccountName: csi-controller-plugin
+      hostPID: true
       containers:
         - name: csi-plugin
           image: &image quay.io/subprovisioner/subprovisioner:v0.1.0
@@ -42,8 +43,6 @@ spec:
               mountPath: /run/csi
             - name: dev
               mountPath: /dev
-            - name: run-lvm
-              mountPath: /run/lvm
           securityContext:
             privileged: true
         - name: csi-provisioner
@@ -67,7 +66,3 @@ spec:
           hostPath:
             path: /dev
             type: Directory
-        - name: run-lvm
-          hostPath:
-            path: /run/subprovisioner/lvm
-            type: DirectoryOrCreate

deploy/02-csi-node-plugin.yaml
@@ -22,6 +22,7 @@ spec:
       labels: *labels
     spec:
       serviceAccountName: csi-node-plugin
+      hostPID: true
       containers:
         - name: csi-plugin
           image: &image quay.io/subprovisioner/subprovisioner:v0.1.0
@@ -42,8 +43,6 @@ spec:
               mountPath: /run/csi
             - name: dev
               mountPath: /dev
-            - name: run-lvm
-              mountPath: /run/lvm
             - name: run-subprovisioner-nbd
               mountPath: /run/subprovisioner/nbd
           securityContext:
@@ -77,10 +76,6 @@ spec:
           hostPath:
             path: /dev
             type: Directory
-        - name: run-lvm
-          hostPath:
-            path: /run/subprovisioner/lvm
-            type: DirectoryOrCreate
         - name: run-subprovisioner-nbd
           hostPath:
             path: /run/subprovisioner/nbd

deploy/03-lvmlockd.yaml
@@ -1,64 +1,0 @@
-# SPDX-License-Identifier: Apache-2.0
-
-apiVersion: apps/v1
-kind: DaemonSet
-metadata:
-  name: lvmlockd
-  namespace: subprovisioner
-spec:
-  selector:
-    matchLabels: &labels
-      subprovisioner.gitlab.io/component: lvmlockd
-  template:
-    metadata:
-      labels: *labels
-    spec:
-      shareProcessNamespace: true
-      containers:
-        - name: lvmlockd
-          image: quay.io/subprovisioner/subprovisioner:v0.1.0
-          command:
-            - scripts/lvmlockd.sh
-            - $(HOST_IP)
-          env:
-            - name: HOST_IP
-              valueFrom:
-                fieldRef:
-                  fieldPath: status.hostIP
-          volumeMounts:
-            - name: dev
-              mountPath: /dev
-            - name: run-lvm
-              mountPath: /run/lvm
-            - name: run-sanlock
-              mountPath: /run/sanlock
-          securityContext:
-            privileged: true
-        - name: sanlock
-          image: quay.io/subprovisioner/subprovisioner:v0.1.0
-          command:
-            - scripts/sanlock.sh
-            - $(NODE_NAME)
-          env:
-            - name: NODE_NAME
-              valueFrom:
-                fieldRef:
-                  fieldPath: spec.nodeName
-          volumeMounts:
-            - name: dev
-              mountPath: /dev
-            - name: run-sanlock
-              mountPath: /run/sanlock
-          securityContext:
-            privileged: true
-      volumes:
-        - name: dev
-          hostPath:
-            path: /dev
-            type: Directory
-        - name: run-lvm
-          hostPath:
-            path: /run/subprovisioner/lvm
-            type: DirectoryOrCreate
-        - name: run-sanlock
-          emptyDir:

commit 02bb087a5469601618476b2b08c053b9d4d1aa61

Do not set DM_DISABLE_UDEV environment variable
Originally the code was designed to run LVM commands from inside the
container and minimize interaction with the host's LVM and udev
infrastructure. Setting DM_DISABLE_UDEV was part of this.

It is no longer necessary to disable udev rules since the switch to the
host's LVM tools. In fact, doing so causes /dev/mapper/<vg-name>-lvmlock
to be removed by udev when a "change" uevent action is received. When
this happens (it is non-deterministic), sanlock fails to open the
lvmlock LV and the tests break.

Allow udev rules so that the /dev/mapper/<vg-name>-lvmlock symlink stays
in place across uevents.

Fixes: 61b29814d1d4 ("Use the node's lvm and sanlock")
Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

pkg/kubesan/util/lvm/lvm.go
@@ -16,10 +16,6 @@ import (
 func Command(ctx context.Context, command string, arg ...string) (string, error) {
 	fullArgs := append([]string{"--target", "1", "--all", "lvm", command}, arg...)
 	cmd := exec.Command("nsenter", fullArgs...)
-
-	cmd.Env = os.Environ()
-	cmd.Env = append(cmd.Env, "DM_DISABLE_UDEV=")
-
 	cmd.Stdin = nil
 
 	stdout, err := cmd.Output()

scripts/lvm.sh
@@ -7,8 +7,6 @@ command=$1
 lvm_vg_name=$2
 lvm_thin_pool_lv_name=$3
 
-export DM_DISABLE_UDEV=
-
 # Usage: __run_ignoring_error <error_regex> <cmd...>
 __run_ignoring_error() {
     local output