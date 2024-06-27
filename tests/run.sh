openshift/kubesan

commit c3dcd4ac56023c856a8b6474ee2c23df10368179

blob: Pass in VG, not raw block device, to StorageClass
Since we recently off-loaded the management of lvmlockd to the nodes
rather than trying to do it within containers (ie. the user must set
up the host id for each node prior to installing subprovisioner), the
next logical step is to require the user to pre-create a shared volume
group and pass that in, rather than expecting subprovisioner to take
over a bare device.

This has a couple of benefits: the user can decide whether to have
multiple VG on a single LUN ('man lvmlockd' recommends that the first
shared VG created be small and dedicated to the global lock, so that
all subsequent shared VGs can be re-homed with less effort as long as
the global lock VG is still around), or even to spread a single shared
VG across multiple lvm PV shared across the nodes.

Also, since the user is now responsible for creating the VG, we can
assume that their VG name is already unambiguous for each node of the
system, and that we no longer need to pass in '--devices /dev/XXX' to
all our lvm calls.

Fixes: https://gitlab.com/subprovisioner/subprovisioner/-/issues/36
Signed-off-by: Eric Blake <eblake@redhat.com>

scripts/lvm.sh
@@ -4,9 +4,8 @@
 set -o errexit -o pipefail -o nounset -o xtrace
 
 command=$1
-backing_device_path=$2
-lvm_vg_name=$3
-lvm_thin_pool_lv_name=$4
+lvm_vg_name=$2
+lvm_thin_pool_lv_name=$3
 
 export DM_DISABLE_UDEV=
 
@@ -24,7 +23,6 @@ __lvm() {
 
 __lockstart() {
     __lvm vgchange \
-        --devices "$backing_device_path" \
         --lock-start \
         "$lvm_vg_name"
 }
@@ -38,8 +36,8 @@ case "${command}" in
         ;;
 
     create-empty)
-        lvm_thin_lv_name=$5
-        size_bytes=$6
+        lvm_thin_lv_name=$4
+        size_bytes=$5
 
         # Start off with a smallish thin pool to avoid wasting space
 
@@ -53,7 +51,6 @@ case "${command}" in
 
         __run_ignoring_error "already exists in volume group" \
             __lvm lvcreate \
-            --devices "$backing_device_path" \
             --activate n \
             --type thin-pool \
             --metadataprofile subprovisioner \
@@ -65,7 +62,6 @@ case "${command}" in
 
         __run_ignoring_error "already exists in volume group" \
             __lvm lvcreate \
-            --devices "$backing_device_path" \
             --type thin \
             --name "$lvm_thin_lv_name" \
             --thinpool "$lvm_thin_pool_lv_name" \
@@ -76,20 +72,18 @@ case "${command}" in
         # --type thin`)
 
         __lvm lvchange \
-            --devices "$backing_device_path" \
             --activate n \
             "$lvm_vg_name/$lvm_thin_lv_name"
         ;;
 
     create-snapshot)
-        lvm_thin_lv_name=$5
-        lvm_source_thin_lv_name=$6
+        lvm_thin_lv_name=$4
+        lvm_source_thin_lv_name=$5
 
         # create snapshot LVM thin LV
 
         __run_ignoring_error "already exists in volume group" \
             __lvm lvcreate \
-            --devices "$backing_device_path" \
             --name "$lvm_thin_lv_name" \
             --snapshot \
             --setactivationskip n \
@@ -99,26 +93,23 @@ case "${command}" in
         # --type thin`)
 
         __lvm lvchange \
-            --devices "$backing_device_path" \
             --activate n \
             "$lvm_vg_name/$lvm_thin_lv_name"
         ;;
 
     delete)
-        lvm_thin_lv_name=$5
+        lvm_thin_lv_name=$4
 
 	    # remove LVM thin LV
 
         __run_ignoring_error "failed to find logical volume" \
             __lvm lvremove \
-            --devices "$backing_device_path" \
             "$lvm_vg_name/$lvm_thin_lv_name"
 
 	    # remove LVM thin *pool* LV if there are no more thin LVs
 
         __run_ignoring_error "failed to find logical volume|removing pool \S+ will remove" \
             __lvm lvremove \
-            --devices "$backing_device_path" \
             "$lvm_vg_name/$lvm_thin_pool_lv_name"
         ;;
 
@@ -126,7 +117,6 @@ case "${command}" in
         # activate LVM thin *pool* LV
 
         __lvm lvchange \
-            --devices "$backing_device_path" \
             --activate ey \
             "$lvm_vg_name/$lvm_thin_pool_lv_name"
         ;;
@@ -135,30 +125,27 @@ case "${command}" in
         # deactivate LVM thin *pool* LV
 
         __lvm lvchange \
-            --devices "$backing_device_path" \
             --activate n \
             "$lvm_vg_name/$lvm_thin_pool_lv_name"
         ;;
 
     activate)
-        lvm_thin_lv_name=$5
+        lvm_thin_lv_name=$4
 
         # activate LVM thin LV
 
         __lvm lvchange \
-            --devices "$backing_device_path" \
             --activate ey \
             --monitor y \
             "$lvm_vg_name/$lvm_thin_lv_name"
         ;;
 
     deactivate)
-        lvm_thin_lv_name=$5
+        lvm_thin_lv_name=$4
 
         # deactivate LVM thin LV
 
         __lvm lvchange \
-            --devices "$backing_device_path" \
             --activate n \
             "$lvm_vg_name/$lvm_thin_lv_name"
         ;;

tests/run.sh
@@ -461,6 +461,7 @@ __run() {
     __log_cyan "Starting NBD server to serve as a shared block device..."
 
     __minikube_ssh "${NODES[0]}" "
+        sudo truncate -s 0 /mnt/vda1/backing.raw
         sudo truncate -s 2G /mnt/vda1/backing.raw
         __run_in_test_container_async --net host -v /mnt/vda1/backing.raw:/disk -- \
             qemu-nbd --cache=none --format=raw --persistent --shared=0 /disk
@@ -498,6 +499,12 @@ __run() {
             sudo systemctl restart sanlock lvmlockd
             "
     done
+
+    __log_cyan "Creating shared VG on controller node..."
+
+    __minikube_ssh "${NODES[0]}" "
+        sudo lvm vgcreate --shared subprovisioner-vg /dev/my-san-lun
+        "
 
     set +o errexit
     (

pkg/subprovisioner/blobs/blobmanager.go
@@ -85,12 +85,11 @@ func (bm *BlobManager) GetBlobSize(ctx context.Context, blob *Blob) (int64, erro
 	output, err := lvm.Command(
 		ctx,
 		"lvs",
-		"--devices", blob.pool.backingDevicePath,
 		"--options", "lv_size",
 		"--units", "b",
 		"--nosuffix",
 		"--noheadings",
-		fmt.Sprintf("%s/%s", config.LvmVgName, blob.lvmThinLvName()),
+		fmt.Sprintf("%s/%s", blob.pool.backingVolumeGroup, blob.lvmThinLvName()),
 	)
 	if err != nil {
 		return -1, status.Errorf(codes.Internal, "failed to get size of LVM LV: %s: %s", err, output)

pkg/subprovisioner/blobs/blob.go
@@ -5,8 +5,6 @@ package blobs
 import (
 	"fmt"
 	"strings"
-
-	"gitlab.com/subprovisioner/subprovisioner/pkg/subprovisioner/util/config"
 )
 
 // Some info describing a particular blob.
@@ -19,12 +17,12 @@ type Blob struct {
 	pool *blobPool
 }
 
-func NewBlob(blobName string, backingDevicePath string) *Blob {
+func NewBlob(blobName string, backingVolumeGroup string) *Blob {
 	return &Blob{
 		name: blobName,
 		pool: &blobPool{
-			name:              blobName,
-			backingDevicePath: backingDevicePath,
+			name:               blobName,
+			backingVolumeGroup: backingVolumeGroup,
 		},
 	}
 }
@@ -34,15 +32,15 @@ func BlobFromString(s string) (*Blob, error) {
 	blob := &Blob{
 		name: split[0],
 		pool: &blobPool{
-			name:              split[1],
-			backingDevicePath: split[2],
+			name:               split[1],
+			backingVolumeGroup: split[2],
 		},
 	}
 	return blob, nil
 }
 
 func (b *Blob) String() string {
-	return fmt.Sprintf("%s:%s:%s", b.name, b.pool.name, b.pool.backingDevicePath)
+	return fmt.Sprintf("%s:%s:%s", b.name, b.pool.name, b.pool.backingVolumeGroup)
 }
 
 func (b *Blob) Name() string {
@@ -54,11 +52,11 @@ func (b *Blob) lvmThinLvName() string {
 }
 
 func (b *Blob) lvmThinLvPath() string {
-	return fmt.Sprintf("/dev/%s/%s", config.LvmVgName, b.lvmThinLvName())
+	return fmt.Sprintf("/dev/%s/%s", b.pool.backingVolumeGroup, b.lvmThinLvName())
 }
 
 func (b *Blob) dmMultipathVolumeName() string {
-	return fmt.Sprintf("%s-%s-dm-multipath", strings.ReplaceAll(config.LvmVgName, "-", "--"), strings.ReplaceAll(b.name, "-", "--"))
+	return fmt.Sprintf("%s-%s-dm-multipath", strings.ReplaceAll(b.pool.backingVolumeGroup, "-", "--"), strings.ReplaceAll(b.name, "-", "--"))
 }
 
 // The returned path is valid on all nodes in the cluster.

pkg/subprovisioner/blobs/blobpool.go
@@ -18,8 +18,8 @@ type blobPool struct {
 	// Matches the name of the pool's Kubernetes BlobPool CRD object.
 	name string
 
-	// Path to the shared block device used as storage for this blob.
-	backingDevicePath string
+	// Name of the shared VG used as storage for this blob.
+	backingVolumeGroup string
 }
 
 type blobPoolCrd struct {

pkg/subprovisioner/blobs/create.go
@@ -6,16 +6,11 @@ import (
 	"context"
 	"fmt"
 	"strconv"
-	"time"
 
 	"gitlab.com/subprovisioner/subprovisioner/pkg/subprovisioner/util/config"
-	"gitlab.com/subprovisioner/subprovisioner/pkg/subprovisioner/util/lvm"
 	"gitlab.com/subprovisioner/subprovisioner/pkg/subprovisioner/util/slices"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-	storagev1 "k8s.io/api/storage/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/util/retry"
 )
 
 // Creates an empty blob.
@@ -27,13 +22,6 @@ func (bm *BlobManager) CreateBlobEmpty(ctx context.Context, blob *Blob, k8sStora
 	// create CRD
 
 	err := bm.createBlobPoolCrd(ctx, blob.pool.name, &blobPoolCrdSpec{Blobs: []string{blob.name}})
-	if err != nil {
-		return err
-	}
-
-	// ensure LVM VG exists
-
-	err = bm.createLvmVg(ctx, k8sStorageClassName, blob.pool.backingDevicePath)
 	if err != nil {
 		return err
 	}
@@ -93,84 +81,6 @@ func (bm *BlobManager) CreateBlobCopy(ctx context.Context, blobName string, sour
 	}
 
 	return blob, nil
-}
-
-func (bm *BlobManager) createLvmVg(ctx context.Context, storageClassName string, pvPath string) error {
-	// TODO: This will hang if the CSI controller plugin creating the VG dies. Fix this, maybe using leases.
-	// TODO: Communicate VG creation errors to users through events/status on the SC and PVC.
-
-	storageClasses := bm.clientset.StorageV1().StorageClasses()
-	stateAnnotation := fmt.Sprintf("%s/lvm-vg-state", config.Domain)
-
-	// check VG state
-
-	var sc *storagev1.StorageClass
-	var shouldCreateVg bool
-
-	err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		var err error
-		sc, err = storageClasses.Get(ctx, storageClassName, metav1.GetOptions{})
-		if err != nil {
-			return err
-		}
-
-		switch state, _ := sc.Annotations[stateAnnotation]; state {
-		case "", "creation-failed":
-			// VG wasn't created and isn't being created, try to create it ourselves
-
-			if sc.Annotations == nil {
-				sc.Annotations = map[string]string{}
-			}
-			sc.Annotations[stateAnnotation] = "creating"
-
-			sc, err = storageClasses.Update(ctx, sc, metav1.UpdateOptions{})
-			if err != nil {
-				return err
-			}
-
-			shouldCreateVg = true
-
-		case "creating", "created":
-			shouldCreateVg = false
-		}
-
-		return nil
-	})
-	if err != nil {
-		return err
-	}
-
-	// create VG or wait until it is created
-
-	if shouldCreateVg {
-		_, err = lvm.Command(ctx, "vgcreate", "--lock-type", "sanlock", config.LvmVgName, pvPath)
-
-		if err == nil {
-			sc.Annotations[stateAnnotation] = "created"
-		} else {
-			sc.Annotations[stateAnnotation] = "creation-failed"
-		}
-
-		// don't use ctx so that we don't fail to update the annotation after successfully creating the VG
-		// TODO: This fails if the SC was modified meanwhile, fix this.
-		_, err = storageClasses.Update(context.Background(), sc, metav1.UpdateOptions{})
-		return err
-	} else {
-		// TODO: Watch instead of polling.
-		for {
-			sc, err := storageClasses.Get(ctx, storageClassName, metav1.GetOptions{})
-
-			if err != nil {
-				return err
-			} else if ctx.Err() != nil {
-				return ctx.Err()
-			} else if sc.Annotations[stateAnnotation] == "created" {
-				return nil
-			}
-
-			time.Sleep(1 * time.Second)
-		}
-	}
 }
 
 // Fails if the volume is attached.

pkg/subprovisioner/blobs/scripts.go
@@ -7,7 +7,6 @@ import (
 	"fmt"
 	"strconv"
 
-	"gitlab.com/subprovisioner/subprovisioner/pkg/subprovisioner/util/config"
 	"gitlab.com/subprovisioner/subprovisioner/pkg/subprovisioner/util/jobs"
 	"gitlab.com/subprovisioner/subprovisioner/pkg/subprovisioner/util/util"
 )
@@ -21,8 +20,8 @@ func (bm *BlobManager) runLvmScriptForThinPoolLv(
 		Command: append(
 			[]string{
 				"scripts/lvm.sh", command,
-				blobPool.backingDevicePath,
-				config.LvmVgName, blobPool.lvmThinPoolLvName(),
+				blobPool.backingVolumeGroup,
+				blobPool.lvmThinPoolLvName(),
 			},
 			extraArgs...,
 		),
@@ -45,8 +44,8 @@ func (bm *BlobManager) runLvmScriptForThinLv(
 		Command: append(
 			[]string{
 				"scripts/lvm.sh", command,
-				blob.pool.backingDevicePath,
-				config.LvmVgName, blob.pool.lvmThinPoolLvName(), blob.lvmThinLvName(),
+				blob.pool.backingVolumeGroup,
+				blob.pool.lvmThinPoolLvName(), blob.lvmThinLvName(),
 			},
 			extraArgs...,
 		),

pkg/subprovisioner/csi/controller/volume.go
@@ -100,7 +100,7 @@ func (s *ControllerServer) createVolume(ctx context.Context, req *csi.CreateVolu
 	if err != nil {
 		return nil, err
 	}
-	backingDevicePath, err := getParameter("backingDevicePath")
+	backingVolumeGroup, err := getParameter("backingVolumeGroup")
 	if err != nil {
 		return nil, err
 	}
@@ -117,7 +117,7 @@ func (s *ControllerServer) createVolume(ctx context.Context, req *csi.CreateVolu
 
 	// create blob
 
-	blob := blobs.NewBlob(pvName, backingDevicePath)
+	blob := blobs.NewBlob(pvName, backingVolumeGroup)
 
 	err = s.BlobManager.CreateBlobEmpty(ctx, blob, *pvc.Spec.StorageClassName, capacity)
 	if err != nil {

commit 25021b99d8c0162abf8fcd0af2b53429c8c663f2

Use --devicesfile to restrict LVM to required PVs only
The migrate.sh test fails because `lvm vgchange --lockstart
subprovisioner-vg` hangs while trying to scan the disconnected
dm-multipath block device.

This used to work because `--devices` prevented LVM from scanning all
block devices including the disconnected dm-multipath block device.

This time around only the LVM Volume Group name is known since the
StorageClass no longer indicates specific block device paths. Achieve
equivalent behavior by adding `--devicesfile` to LVM command-lines. The
corresponding file in /etc/lvm/devices/ must be generated using
vgimportdevices(8) by the user before creating the StorageClass.

Now LVM commands only looks at the block devices actually backing the
Volume Group. Migration no longer gets stuck waiting on a dm-multipath
device that will only become available once migration has completed.

Fixes: c3dcd4ac5602 ("blob: Pass in VG, not raw block device, to StorageClass")
Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

pkg/kubesan/blobs/blobmanager.go
@@ -85,6 +85,7 @@ func (bm *BlobManager) GetBlobSize(ctx context.Context, blob *Blob) (int64, erro
 	output, err := lvm.Command(
 		ctx,
 		"lvs",
+		"--devicesfile", blob.pool.backingVolumeGroup,
 		"--options", "lv_size",
 		"--units", "b",
 		"--nosuffix",

scripts/lvm.sh
@@ -21,6 +21,7 @@ __lvm() {
 
 __lockstart() {
     __lvm vgchange \
+        --devicesfile "$lvm_vg_name" \
         --lock-start \
         "$lvm_vg_name"
 }
@@ -49,6 +50,7 @@ case "${command}" in
 
         __run_ignoring_error "already exists in volume group" \
             __lvm lvcreate \
+            --devicesfile "$lvm_vg_name" \
             --activate n \
             --type thin-pool \
             --metadataprofile kubesan \
@@ -60,6 +62,7 @@ case "${command}" in
 
         __run_ignoring_error "already exists in volume group" \
             __lvm lvcreate \
+            --devicesfile "$lvm_vg_name" \
             --type thin \
             --name "$lvm_thin_lv_name" \
             --thinpool "$lvm_thin_pool_lv_name" \
@@ -70,6 +73,7 @@ case "${command}" in
         # --type thin`)
 
         __lvm lvchange \
+            --devicesfile "$lvm_vg_name" \
             --activate n \
             "$lvm_vg_name/$lvm_thin_lv_name"
         ;;
@@ -82,6 +86,7 @@ case "${command}" in
 
         __run_ignoring_error "already exists in volume group" \
             __lvm lvcreate \
+            --devicesfile "$lvm_vg_name" \
             --name "$lvm_thin_lv_name" \
             --snapshot \
             --setactivationskip n \
@@ -91,6 +96,7 @@ case "${command}" in
         # --type thin`)
 
         __lvm lvchange \
+            --devicesfile "$lvm_vg_name" \
             --activate n \
             "$lvm_vg_name/$lvm_thin_lv_name"
         ;;
@@ -102,12 +108,14 @@ case "${command}" in
 
         __run_ignoring_error "failed to find logical volume" \
             __lvm lvremove \
+            --devicesfile "$lvm_vg_name" \
             "$lvm_vg_name/$lvm_thin_lv_name"
 
 	    # remove LVM thin *pool* LV if there are no more thin LVs
 
         __run_ignoring_error "failed to find logical volume|removing pool \S+ will remove" \
             __lvm lvremove \
+            --devicesfile "$lvm_vg_name" \
             "$lvm_vg_name/$lvm_thin_pool_lv_name"
         ;;
 
@@ -115,6 +123,7 @@ case "${command}" in
         # activate LVM thin *pool* LV
 
         __lvm lvchange \
+            --devicesfile "$lvm_vg_name" \
             --activate ey \
             "$lvm_vg_name/$lvm_thin_pool_lv_name"
         ;;
@@ -123,6 +132,7 @@ case "${command}" in
         # deactivate LVM thin *pool* LV
 
         __lvm lvchange \
+            --devicesfile "$lvm_vg_name" \
             --activate n \
             "$lvm_vg_name/$lvm_thin_pool_lv_name"
         ;;
@@ -133,6 +143,7 @@ case "${command}" in
         # activate LVM thin LV
 
         __lvm lvchange \
+            --devicesfile "$lvm_vg_name" \
             --activate ey \
             --monitor y \
             "$lvm_vg_name/$lvm_thin_lv_name"
@@ -144,6 +155,7 @@ case "${command}" in
         # deactivate LVM thin LV
 
         __lvm lvchange \
+            --devicesfile "$lvm_vg_name" \
             --activate n \
             "$lvm_vg_name/$lvm_thin_lv_name"
         ;;

tests/run.sh
@@ -535,6 +535,15 @@ __run() {
         sudo lvm vgcreate --shared kubesan-vg /dev/my-san-lun
         "
 
+    __log_cyan "Creating LVM devices files on nodes..."
+
+    for node in "${NODES[@]}"; do
+        __minikube_ssh "${node}" "
+        sudo vgchange --lockstart kubesan-vg
+        sudo vgimportdevices kubesan-vg --devicesfile kubesan-vg
+        "
+    done
+
     set +o errexit
     (
         set -o errexit -o pipefail -o nounset +o xtrace