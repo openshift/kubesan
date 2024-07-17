7749dfdc1f1260c5165d9c4e237651c0cbff96ed
Jakob Möller <jmoller@redhat.com>
2024-07-17 14:40:35 +0200

    chore: always cleanup mount point and rely on internal checks

--- pkg/kubesan/csi/node/stage.go
+++ pkg/kubesan/csi/node/stage.go
@@ -4,6 +4,8 @@ package node
 
 import (
 	"context"
+	"fmt"
+	"log"
 
 	"github.com/container-storage-interface/spec/lib/go/csi"
 	"gitlab.com/kubesan/kubesan/pkg/kubesan/blobs"
@@ -30,7 +32,6 @@ func (s *NodeServer) formatAndMount(source string, target string, fstype string,
 	if err := f.FormatAndMountSensitive(source, target, fstype, nil, mountFlags); err != nil {
 		return status.Errorf(codes.Internal, "format and mount failed source=%s target=%s fstype=%s: %v", source, target, fstype, err)
 	}
-
 
 	// cloned volumes may be larger than the file system, so resize
 
@@ -65,8 +66,11 @@ func (s *NodeServer) NodeStageVolume(ctx context.Context, req *csi.NodeStageVolu
 
 	// attach blob on current node
 
+	log.Println(fmt.Sprintf("Attaching blob %s on current node %s", blob, config.LocalNodeName))
+
 	_, path, err := s.BlobManager.AttachBlob(ctx, blob, &config.LocalNodeName, "staged")
 	if err != nil {
+		log.Println(fmt.Sprintf("Failed to attach blob %s on current node %s: %v", blob, config.LocalNodeName, err))
 		return nil, err
 	}
 
@@ -100,16 +104,9 @@ func (s *NodeServer) NodeUnstageVolume(ctx context.Context, req *csi.NodeUnstage
 	// unmount file system, if necessary
 
 	targetPath := req.GetStagingTargetPath()
-	sure, err := s.mounter.IsMountPoint(targetPath)
-	if err != nil {
-		return nil, status.Errorf(codes.Internal, "failed to determine whether %s is a mount point for volume %s: %v", targetPath, req.VolumeId, err)
+	if err := mount.CleanupMountPoint(targetPath, s.mounter, true); err != nil {
+		return nil, status.Errorf(codes.Internal, "failed to clean up mount point %s for volume %s: %v", targetPath, req.VolumeId, err)
 	}
-	if sure {
-		if err := mount.CleanupMountPoint(targetPath, s.mounter, true); err != nil {
-			return nil, status.Errorf(codes.Internal, "failed to clean up mount point %s for volume %s: %v", targetPath, req.VolumeId, err)
-		}
-	}
-
 	// detach blob from current node
 
 	err = s.BlobManager.DetachBlob(ctx, blob, config.LocalNodeName, "staged")
