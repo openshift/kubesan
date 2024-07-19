5ad85ab4cf35e11103f855573f1802682eedbee1
Fabio M. Di Nitto <fdinitto@redhat.com>
2024-07-19 11:21:20 +0200

    test libs: move __log to separate file
    Signed-off-by: Fabio M. Di Nitto <fdinitto@redhat.com>

--- tests/run.sh
+++ tests/run.sh
@@ -126,51 +126,10 @@ for test in "${tests_arg[@]}"; do
     done
 done
 
-# private definitions
-
-# Usage: __elapsed
-__elapsed() {
-    local delta
-    delta=$(( $( date +%s%N ) - start_time ))
-    printf '%d.%09d' $(( delta / 10**9 )) $(( delta % 10**9 ))
-}
-
-# Usage: __big_log <color> <format> <args...>
-__big_log() {
-    local text term_cols sep_len
-    text="$( printf "${@:2}" )"
-    term_cols="$( tput cols 2> /dev/null )" || term_cols=80
-    sep_len="$(( term_cols - ${#text} - 16 ))"
-    printf "\033[%sm--- [%6.1f] %s " "$1" "$( __elapsed )" "${text}"
-    printf '%*s\033[0m\n' "$(( sep_len < 0 ? 0 : sep_len ))" '' | tr ' ' -
-}
-
-# Usage: __log <color> <format> <args...>
-__log() {
-    # shellcheck disable=SC2059
-    printf "\033[%sm--- [%6.1f] %s\033[0m\n" \
-        "$1" "$( __elapsed )" "$( printf "${@:2}" )"
-}
-
-# Usage: __log_red <format> <args...>
-__log_red() {
-    __log 31 "$@"
-}
-
-# Usage: __log_green <format> <args...>
-__log_green() {
-    __log 32 "$@"
-}
-
-# Usage: __log_yellow <format> <args...>
-__log_yellow() {
-    __log 33 "$@"
-}
-
-# Usage: __log_cyan <format> <args...>
-__log_cyan() {
-    __log 36 "$@"
-}
+for f in ${script_dir}/lib/*.sh; do
+    # shellcheck disable=SC1090
+    source "$f"
+done
 
 __shell() {
     __log "$1" 'Starting interactive shell.'
@@ -220,33 +179,10 @@ __shell() {
     ) || true
 }
 
-# Usage: __failure <format> <args...>
-__failure() {
-    __log_red "$@"
-
-    if (( pause_on_failure )); then
-        __shell 31 false
-    fi
-}
-
-# Usage: __canceled <format> <args...>
-__canceled() {
-    __log_yellow "$@"
-
-    if (( pause_on_failure )); then
-        __shell 33 false
-    fi
-}
-
 # definitions shared with test scripts
 
 export REPO_ROOT=${repo_root}
 export TEST_IMAGE=docker.io/localhost/kubesan/test:test
-
-for f in ${script_dir}/lib/*.sh; do
-    # shellcheck disable=SC1090
-    source "$f"
-done
 
 # build images
 
