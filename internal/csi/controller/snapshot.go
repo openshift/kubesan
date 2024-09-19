openshift/kubesan

commit 0d52fdb1a64174d28d64aa40bb315f1681a2e975

api: Prefer Namespaced CRDs
Our use of CRDs is to assist the work of KubeSAN.  We don't want our
CRD names to pollute the global cluster namespace, particularly for a
CRD based on a common word like Volume.  This change means that
'kubectl get volumes' no longer works, and you now need 'kubectl
--namespace kubesan-system get volumes'; but I don't see that as a
drawback.

Related to that, our RBAC can use merely Role objects tied to the
namespace, rather than ClusterRole objects.  This gives us a bit more
security in that pods outside the namespace won't be manipulating our
resources.

Note that before the crds branch, we were using the 'kubesan'
namespace; kubebuilder defaults to giving us the 'kubesan-system'
namespace.  We may still want to tweak that in a followup patch.
Along those lines,
https://sdk.operatorframework.io/docs/building-operators/golang/operator-scope/
recommends writing the controller binary to accept the environment
variable WATCH_NAMESPACE, and setting that in
config/manager/manager.yaml to the chosen namespace, rather than
hard-coding it.  But for now, hard-coding is good enough.

Signed-off-by: Eric Blake <eblake@redhat.com>

internal/csi/controller/snapshot.go
@@ -31,7 +31,8 @@ func (s *ControllerServer) CreateSnapshot(ctx context.Context, req *csi.CreateSn
 
 	snapshot := &v1alpha1.Snapshot{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: req.Name,
+			Name:      req.Name,
+			Namespace: "kubesan-system",
 		},
 		Spec: v1alpha1.SnapshotSpec{
 			SourceVolume: req.SourceVolumeId,

internal/csi/controller/volume.go
@@ -55,7 +55,8 @@ func (s *ControllerServer) CreateVolume(ctx context.Context, req *csi.CreateVolu
 
 	volume := &v1alpha1.Volume{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: req.Name,
+			Name:      req.Name,
+			Namespace: "kubesan-system",
 		},
 		Spec: v1alpha1.VolumeSpec{
 			VgName:      lvmVolumeGroup,

api/v1alpha1/deviceswitch_types.go
@@ -25,7 +25,7 @@ type DeviceSwitchStatus struct {
 }
 
 // +kubebuilder:object:root=true
-// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:resource:scope=Namespaced
 // +kubebuilder:subresource:status
 
 type DeviceSwitch struct {

api/v1alpha1/nbdserver_types.go
@@ -16,7 +16,7 @@ type NbdServerStatus struct {
 }
 
 // +kubebuilder:object:root=true
-// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:resource:scope=Namespaced
 // +kubebuilder:subresource:status
 
 type NbdServer struct {

api/v1alpha1/snapshot_types.go
@@ -29,7 +29,7 @@ type SnapshotStatus struct {
 }
 
 // +kubebuilder:object:root=true
-// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:resource:scope=Namespaced
 // +kubebuilder:subresource:status
 
 type Snapshot struct {

api/v1alpha1/thinblob_types.go
@@ -100,7 +100,7 @@ type ThinBlobAttachmentStateResuming struct {
 }
 
 // +kubebuilder:object:root=true
-// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:resource:scope=Namespaced
 // +kubebuilder:subresource:status
 
 type ThinBlob struct {

api/v1alpha1/thinpool_types.go
@@ -54,6 +54,7 @@ type ThinPoolStatus struct {
 }
 
 // +kubebuilder:object:root=true
+// +kubebuilder:resource:scope=Namespaced
 // +kubebuilder:subresource:status
 
 // ThinPool is the Schema for the thinpools API

api/v1alpha1/thinpoollv_types.go
@@ -126,7 +126,7 @@ type ThinLvStateActive struct {
 }
 
 // +kubebuilder:object:root=true
-// +kubebuilder:resource:scope=Cluster
+// +kubebuilder:resource:scope=Namespaced
 // +kubebuilder:subresource:status
 
 type ThinPoolLv struct {

commit 1f9ecef0ab018863447893dc6fdb1157b27432ea

csi: Use namespace for deletion, too
Namespace is important for more than just creation. Without this, we
leak storage when a PVC is deleted.

Fixes: 0d52fdb1a
Signed-off-by: Eric Blake <eblake@redhat.com>

internal/csi/controller/snapshot.go
@@ -78,7 +78,8 @@ func (s *ControllerServer) DeleteSnapshot(ctx context.Context, req *csi.DeleteSn
 
 	snapshot := &v1alpha1.Snapshot{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: req.SnapshotId,
+			Name:      req.SnapshotId,
+			Namespace: config.Namespace,
 		},
 	}
 

internal/csi/controller/volume.go
@@ -229,7 +229,8 @@ func (s *ControllerServer) DeleteVolume(ctx context.Context, req *csi.DeleteVolu
 
 	volume := &v1alpha1.Volume{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: req.VolumeId,
+			Name:      req.VolumeId,
+			Namespace: config.Namespace,
 		},
 	}
 