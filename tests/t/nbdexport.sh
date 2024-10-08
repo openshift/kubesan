64d39974cf5548ae3c0e9ed34503ef338b8b216b
Eric Blake <eblake@redhat.com>
2024-10-08 14:03:55 -0500

    crd: Add unit test for NbdExport CRD
    Normal users will never manage this resource directly; but having a
    unit test that exports a dummy volume and proves that the resource
    lasts until all clients are gone is worthwhile even before wiring up
    Volume code to utilize the CRD.  The test should be enhanced to
    connect an NBD client to the export once the code is implemented to
    serve things, but for now proves that the CR lifecycle works (in
    particular, we can modify .Spec after an export has been marked for
    deletion, and the .Status.Conditions reflect whether it is safe to
    connect).
    
    Signed-off-by: Eric Blake <eblake@redhat.com>

---
+++ tests/t/nbdexport.sh
@@ -0,0 +1,40 @@
+# SPDX-License-Identifier: Apache-2.0
+
+ksan-stage "Creating NbdExport..."
+
+kubectl create -f - <<EOF
+apiVersion: kubesan.gitlab.io/v1alpha1
+kind: NbdExport
+metadata:
+  name: export
+  namespace: kubesan-system
+spec:
+  source: "/dev/null"
+  host: $(__ksan-get-node-name 0)
+EOF
+
+# Wait for Status.Conditions["Available"]
+ksan-poll 1 30 "kubectl get --namespace kubesan-system -o=jsonpath='{.status.conditions[*]['\''type'\'','\''status'\'']}' nbdexport export | grep --quiet 'Available True'"
+
+# TODO Connect a temporary NBD client to the export, to prove that CR
+# creation does trigger the correct access.
+
+ksan-stage "Adding client..."
+kubectl patch --namespace kubesan-system nbdexport export --type merge -p "
+spec:
+  clients:
+    - $(__ksan-get-node-name 1)
+"
+
+ksan-stage "Deleting export..."
+kubectl delete --namespace kubesan-system --wait=false nbdexport export
+ksan-poll 1 30 "kubectl get --namespace kubesan-system -o=jsonpath='{.status.conditions[*]['\''type'\'','\''status'\'']}' nbdexport export | grep --quiet 'Available False'"
+
+ksan-stage "Dropping client..."
+kubectl patch --namespace kubesan-system nbdexport export --type merge -p "
+spec:
+  clients: []
+"
+ksan-poll 1 30 "[[ -z \"\$(kubectl get --no-headers --namespace kubesan-system nbdexport 2>/dev/null)\" ]]"
+
+ksan-stage "Cleaning up..."
