openshift/kubesan

commit bc6b4a4083cef249ebc8173a18b22217206dde4c

manager: Zero Linear LVs upon creation
The contents of newly created linear LVs are undefined. This can be a
security problem when untrusted users have access to the same
StorageClass because users can see the previous contents of the disk by
creating new LVs.

This behavior also triggers occassional issues with Filesystem volumes
where formatAndMount() skips mkfs if there is already a file system
present. In one instance mkfs was skipped but actually mounting the
volume failed because it contained a Master Boot Record rather than a
single file system.

Solve these issues by zeroing new linear LVs using blkdiskcard(8) with
the --zeroout option. Doing this idempotently involves tracking whether
or not blkdiscard was already run successfully. Use the LVM tags
mechanism to store this state along with the LV.

Fixes: #63
Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

internal/common/commands/commands.go
@@ -167,6 +167,48 @@ func LvmLvRemoveIdempotent(args ...string) (Output, error) {
 	return output, err
 }
 
+func LvmLvHasTag(vgName string, lvName string, tag string) (bool, error) {
+	output, err := Lvm(
+		"lvs",
+		"--devicesfile", vgName,
+		"--select", fmt.Sprintf("lv_tags = {\"%s\"}", tag),
+		fmt.Sprintf("%s/%s", vgName, lvName),
+	)
+	if err != nil {
+		return false, err
+	}
+
+	// there is output only if the tag is present
+	return string(output.Combined) != "", nil
+}
+
+func LvmLvAddTag(vgName string, lvName string, tag string) error {
+	// lvchange succeeds if the tag is already present
+	_, err := Lvm(
+		"lvchange",
+		"--devicesfile", vgName,
+		"--addtag", tag,
+		fmt.Sprintf("%s/%s", vgName, lvName),
+	)
+	return err
+}
+
+// Calls a function with an LV activated temporarily
+func WithLvmLvActivated(vgName string, lvName string, op func() error) (err error) {
+	vgLvName := fmt.Sprintf("%s/%s", vgName, lvName)
+
+	_, err = Lvm("lvchange", "--activate", "y", vgLvName)
+	if err != nil {
+		return err
+	}
+
+	defer func() {
+		_, err = Lvm("lvchange", "--activate", "n", vgLvName)
+	}()
+
+	return op()
+}
+
 var (
 	nbdClientConnectedPattern = regexp.MustCompile(`^Connected (/dev/\S*)`)
 )

internal/manager/cluster/blob_manager_linear.go
@@ -6,6 +6,8 @@ import (
 	"context"
 	"fmt"
 
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
 	"gitlab.com/kubesan/kubesan/internal/common/commands"
 )
 
@@ -25,6 +27,8 @@ func NewLinearBlobManager(vgName string) BlobManager {
 }
 
 func (m *LinearBlobManager) CreateBlob(ctx context.Context, name string, sizeBytes int64) error {
+	log := log.FromContext(ctx)
+
 	_, err := commands.LvmLvCreateIdempotent(
 		"--devicesfile", m.vgName,
 		"--activate", "n",
@@ -34,15 +38,41 @@ func (m *LinearBlobManager) CreateBlob(ctx context.Context, name string, sizeByt
 		"--size", fmt.Sprintf("%db", sizeBytes),
 		m.vgName,
 	)
+	if err != nil {
+		return err
+	}
 
 	// TODO recreate if size does not match. This handles the case where a
 	// blob was partially created and then reconciled again with a
 	// different size. A blob must never be recreated after volume creation
 	// has completed since that could lose data!
 
-	// TODO zero the LV (linear LVs are not automatically zero-initialized)
-	// https://gitlab.com/kubesan/kubesan/-/issues/63
-	return err
+	// Linear volumes contain the previous contents of the disk, which can
+	// be an information leak if multiple users have access to the same
+	// Volume Group. Zero the LV to avoid security issues.
+	LvmLvTagZeroed := "kubesan.gitlab.io/zeroed=true"
+	hasTag, err := commands.LvmLvHasTag(m.vgName, name, LvmLvTagZeroed)
+	if err != nil {
+		return err
+	}
+	if !hasTag {
+		err = commands.WithLvmLvActivated(m.vgName, name, func() error {
+			path := fmt.Sprintf("/dev/%s/%s", m.vgName, name)
+			log.Info("CreateBlob zeroing LV", "path", path)
+			_, err := commands.RunOnHost("blkdiscard", "--zeroout", path)
+			return err
+		})
+		if err != nil {
+			return err
+		}
+
+		err = commands.LvmLvAddTag(m.vgName, name, LvmLvTagZeroed)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func (m *LinearBlobManager) RemoveBlob(ctx context.Context, name string) error {

commit 565379e1c138106fe451b98f42d03a29563acad2

commands: Fix missing --devicesfile in Linear LV zeroing
Remember to use --devicesfile <vg-name> so that LVM sees only the
LVM Physical Volumes (PVs) containing the Volume Group.

This fixes an error during Linear LV creation when nodes have the LVM
PVs only in the VG-specific devices file:

  command "nsenter --target 1 --all lvm lvchange --activate y my-vg/pvc-d3a3caae-e50f-4e27-bd92-a6f6d9c02187" failed with exit code 5:   Volume group "my-vg" not found
  Cannot process volume group my-vg

Fixes: bc6b4a4083ce ("manager: Zero Linear LVs upon creation")
Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

internal/common/commands/commands.go
@@ -217,13 +217,13 @@ func LvmLvAddTag(vgName string, lvName string, tag string) error {
 func WithLvmLvActivated(vgName string, lvName string, op func() error) (err error) {
 	vgLvName := fmt.Sprintf("%s/%s", vgName, lvName)
 
-	_, err = Lvm("lvchange", "--activate", "y", vgLvName)
+	_, err = Lvm("lvchange", "--devicesfile", vgName, "--activate", "y", vgLvName)
 	if err != nil {
 		return err
 	}
 
 	defer func() {
-		_, err = Lvm("lvchange", "--activate", "n", vgLvName)
+		_, err = Lvm("lvchange", "--devicesfile", vgName, "--activate", "n", vgLvName)
 	}()
 
 	return op()