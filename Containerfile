openshift/kubesan

commit 37e42839693b6fa5cd37b63f979446c4b7bb2ec9

Makefile, Containerfile: run makefile as part of container build
The CI Pipeline was broken because it didn't have native go installed
when it ran the Makefile, which uses go to build artifacts. This patch
runs the Makefile as part of the container build and uses the Container's
go version for consistency. The containerized-build script runs the
commands to build everything in the Container.

It also fixes a bug in the Makefile where the controller-gen target
wasn't running.

Signed-off-by: Chandan Somani <csomani@redhat.com>

Containerfile
@@ -1,17 +1,14 @@
 # SPDX-License-Identifier: Apache-2.0
 
-FROM quay.io/projectquay/golang:1.22 AS builder
+FROM quay.io/projectquay/golang:1.22 AS makefile
+
+RUN dnf install -qy diffutils && dnf clean all
 
 WORKDIR /kubesan
 
-COPY go.mod go.sum ./
-RUN go mod download
+COPY ./ ./
 
-COPY api/ api/
-COPY cmd/ cmd/
-COPY internal/ internal/
-
-RUN go build -o bin/kubesan cmd/main.go
+RUN make build
 
 # CentOS Stream 9 doesn't provide package nbd
 # FROM quay.io/centos/centos:stream9
@@ -23,6 +20,6 @@ RUN dnf install -qy nbd qemu-img util-linux-core e2fsprogs xfsprogs && dnf clean
 
 WORKDIR /kubesan
 
-COPY --from=builder /kubesan/bin/kubesan bin/
+COPY --from=makefile /kubesan/bin/kubesan bin/
 
 ENTRYPOINT [ "/kubesan/bin/kubesan" ]

Makefile
@@ -5,14 +5,26 @@ IMG ?= kubesan:latest
 # ENVTEST_K8S_VERSION refers to the version of kubebuilder assets to be downloaded by envtest binary.
 ENVTEST_K8S_VERSION = 1.29.0
 
+## Location to install dependencies to
+LOCALBIN ?= $(shell pwd)/bin
+
+## Tool Versions
+CONTROLLER_TOOLS_VERSION ?= v0.14.0
+ENVTEST_VERSION ?= release-0.17
+GOLANGCI_LINT_VERSION ?= v1.57.2
+
+## Tool Binaries
+KUBECTL ?= kubectl
+CONTROLLER_GEN ?= $(LOCALBIN)/controller-gen-$(CONTROLLER_TOOLS_VERSION)
+ENVTEST ?= $(LOCALBIN)/setup-envtest-$(ENVTEST_VERSION)
+GOLANGCI_LINT = $(LOCALBIN)/golangci-lint-$(GOLANGCI_LINT_VERSION)
+
 # Get the currently used golang install path (in GOPATH/bin, unless GOBIN is set)
 ifeq (,$(shell go env GOBIN))
 GOBIN=$(shell go env GOPATH)/bin
 else
 GOBIN=$(shell go env GOBIN)
 endif
-
-CONTAINER_TOOL ?= podman
 
 # Setting SHELL to bash allows bash commands to be executed by recipes.
 # Options are set to exit when a recipe line exits non-zero or a piped command fails.
@@ -43,7 +55,8 @@ help: ## Display this help.
 
 .PHONY: build
 build: .generate.timestamp vet lint ## Build the KubeSAN image.
-	$(CONTAINER_TOOL) build -t ${IMG} .
+	go mod download
+	go build -o bin/kubesan cmd/main.go
 
 ##@ Development
 
@@ -89,21 +102,8 @@ lint-fix: golangci-lint ## Run the golangci-lint linter and perform fixes.
 
 ##@ Dependencies
 
-## Location to install dependencies to
-LOCALBIN ?= $(shell pwd)/bin
 $(LOCALBIN):
 	mkdir -p $(LOCALBIN)
-
-## Tool Binaries
-KUBECTL ?= kubectl
-CONTROLLER_GEN ?= $(LOCALBIN)/controller-gen-$(CONTROLLER_TOOLS_VERSION)
-ENVTEST ?= $(LOCALBIN)/setup-envtest-$(ENVTEST_VERSION)
-GOLANGCI_LINT = $(LOCALBIN)/golangci-lint-$(GOLANGCI_LINT_VERSION)
-
-## Tool Versions
-CONTROLLER_TOOLS_VERSION ?= v0.14.0
-ENVTEST_VERSION ?= release-0.17
-GOLANGCI_LINT_VERSION ?= v1.57.2
 
 .PHONY: controller-gen
 controller-gen: $(CONTROLLER_GEN) ## Download controller-gen.

containerized-build.sh
@@ -1,0 +1,6 @@
+# SPDX-License-Identifier: Apache-2.0
+
+set -e -x
+
+#Build kubesan image
+podman build -t kubesan:latest .

commit 27a2ab4e1d7a738e20fa3d5c06c2fcc939edcdc9

build: Fix tests/run.sh after local `make` run
In a CI environment, when nothing exists locally other than what was
in git, the containerized-build.sh script using "COPY ./ ./" is just
fine, since running "make" is going to build the world the first time.
But in local development, when ./bin has been created after "make",
when tests/run.sh tries to build a container, the "COPY ./ ./" copies
in binaries built for the host libc, but then tries to execute them in
the (often intentionally-older) libc of the container environment,
which fails if the binary depends on newer glibc symbols.

Fix this by splitting up explicit COPY lines in the Containerfile.
While at it, explicitly run a few steps earlier than the COPY so that
we can cache the results that are more likely to be slow-changing
(such as populating ./bin) while still reacting to development in the
final "RUN make build".

Someday, we may want to pre-build a container with controller-gen and
golangci-lint in that container, then run this container with "make
CONTROLLER_GEN=... GOLANGCI_LINT=..." pointing to those tools from the
imported container, to further reduce the amount of work needed in CI,
but that's not the goal of this patch.

Fixes: 37e42839 ("Makefile, Containerfile: run makefile as part of container build")
Signed-off-by: Eric Blake <eblake@redhat.com>

Containerfile
@@ -6,7 +6,27 @@ RUN dnf install -qy diffutils && dnf clean all
 
 WORKDIR /kubesan
 
-COPY ./ ./
+# Although "make" will also do "go mod download", doing it as a separate
+# RUN now lets us cache things for faster container reuse
+COPY go.mod go.sum Makefile ./
+RUN go mod download
+
+# Although "make" will also build these tools, pre-building them as a
+# separate RUN now lets us cache things for faster container reuse
+RUN make controller-gen golangci-lint
+
+# Copy only source files, not generated binaries, since binaries built
+# in the host environment may not work in the container environment.
+# If .generate.timestamp exists in the host, we assume that the files
+# generated by controller-gen are already up-to-date and don't need to
+# be rebuilt; use of the ? allows the glob to pass if the file does not
+# exist, such as in a CI environment on a fresh checkout.
+COPY PROJECT .generate?timestamp ./
+COPY api/ api/
+COPY cmd/ cmd/
+COPY deploy/ deploy/
+COPY hack/ hack/
+COPY internal/ internal/
 
 RUN make build
 