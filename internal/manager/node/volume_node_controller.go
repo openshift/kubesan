openshift/kubesan

commit 76a46ad4d7fa8ea44ba368e39c7dac61e7338e05

volume: Wait for volume activation with WatchPending
The Volume Node reconciler is already watching for changes to
ThinPoolLv, so during a node stage operation, a reconcile may happen
more than once (on the first pass, we update ThinPoolLv but don't see
it active, so updateStatusAttachedToNodes changes nothing; on the
second pass, we have nothing to change in ThinPoolLv but can now
update our status to match the changed status that triggered our
second reconcile).  But this is not as clean as having
reconcileThinAttaching do the waiting, and do it by use of the
WatchPending helper error, especially since that will make the next
patch to add in device-mapper wrappers easier.

Signed-off-by: Eric Blake <eblake@redhat.com>

internal/manager/node/volume_node_controller.go
@@ -21,6 +21,7 @@ import (
 	"gitlab.com/kubesan/kubesan/internal/common/config"
 	kubesanslices "gitlab.com/kubesan/kubesan/internal/common/slices"
 	"gitlab.com/kubesan/kubesan/internal/manager/common/thinpoollv"
+	"gitlab.com/kubesan/kubesan/internal/manager/common/util"
 )
 
 type VolumeNodeReconciler struct {
@@ -44,6 +45,7 @@ func SetUpVolumeNodeReconciler(mgr ctrl.Manager) error {
 // +kubebuilder:rbac:groups=kubesan.gitlab.io,resources=volumes/status,verbs=get;update;patch,namespace=kubesan-system
 
 // Ensure that the volume is attached to this node
+// May fail with WatchPending if another reconcile will trigger progress
 func (r *VolumeNodeReconciler) reconcileThinAttaching(ctx context.Context, volume *v1alpha1.Volume, thinPoolLv *v1alpha1.ThinPoolLv) error {
 	oldThinPoolLv := thinPoolLv.DeepCopy()
 
@@ -85,7 +87,15 @@ func (r *VolumeNodeReconciler) reconcileThinDetaching(ctx context.Context, volum
 		}
 	}
 
-	return thinpoollv.UpdateThinPoolLv(ctx, r.Client, thinPoolLv, oldThinPoolLv != thinPoolLv)
+	if err := thinpoollv.UpdateThinPoolLv(ctx, r.Client, thinPoolLv, oldThinPoolLv != thinPoolLv); err != nil {
+		return err
+	}
+
+	if !isThinLvActiveOnLocalNode(thinPoolLv, thinLvName) {
+		return &util.WatchPending{}
+	}
+
+	return nil
 }
 
 func isThinLvActiveOnLocalNode(thinPoolLv *v1alpha1.ThinPoolLv, name string) bool {
@@ -129,19 +139,25 @@ func (r *VolumeNodeReconciler) updateStatusAttachedToNodes(ctx context.Context, 
 
 func (r *VolumeNodeReconciler) reconcileThin(ctx context.Context, volume *v1alpha1.Volume) error {
 	thinPoolLv := &v1alpha1.ThinPoolLv{}
+	log := log.FromContext(ctx).WithValues("nodeName", config.LocalNodeName)
 
-	if err := r.Get(ctx, types.NamespacedName{Name: volume.Name, Namespace: config.Namespace}, thinPoolLv); err != nil {
+	err := r.Get(ctx, types.NamespacedName{Name: volume.Name, Namespace: config.Namespace}, thinPoolLv)
+	if err != nil {
 		return client.IgnoreNotFound(err)
 	}
 
 	if slices.Contains(volume.Spec.AttachToNodes, config.LocalNodeName) {
-		if err := r.reconcileThinAttaching(ctx, volume, thinPoolLv); err != nil {
-			return err
-		}
+		err = r.reconcileThinAttaching(ctx, volume, thinPoolLv)
 	} else {
-		if err := r.reconcileThinDetaching(ctx, volume, thinPoolLv); err != nil {
-			return err
+		err = r.reconcileThinDetaching(ctx, volume, thinPoolLv)
+	}
+
+	if err != nil {
+		if _, ok := err.(*util.WatchPending); ok {
+			log.Info("reconcileThin waiting for Watch")
+			return nil // wait until Watch triggers
 		}
+		return err
 	}
 
 	return r.updateStatusAttachedToNodes(ctx, volume, thinPoolLv)

commit e8452147e79954cbde305f8d421963709e2c7f77

volume: Fix mismerge with dm activation
Commit 76a46ad4 was supposed to add a WatchPending failure to
reconcileThinAttaching(), but due to some merge conflicts encountered
while rebasing, it ended up adding the condition to
reconcileThinDetatching() instead (where the logic is wrong - when
detaching, we expect the LV to be deactivated soon).  Later, commit
671520af added the intended WatchPending check in the right place, but
without noticing that the wrong place was still intact.  Clean up the
mess now.

Fixes: 76a46ad4 ("volume: Wait for volume activation with WatchPending")
Signed-off-by: Eric Blake <eblake@redhat.com>

internal/manager/node/volume_node_controller.go
@@ -104,15 +104,7 @@ func (r *VolumeNodeReconciler) reconcileThinDetaching(ctx context.Context, volum
 		}
 	}
 
-	if err := thinpoollv.UpdateThinPoolLv(ctx, r.Client, thinPoolLv, oldThinPoolLv != thinPoolLv); err != nil {
-		return err
-	}
-
-	if !isThinLvActiveOnLocalNode(thinPoolLv, thinLvName) {
-		return &util.WatchPending{}
-	}
-
-	return nil
+	return thinpoollv.UpdateThinPoolLv(ctx, r.Client, thinPoolLv, oldThinPoolLv != thinPoolLv)
 }
 
 func isThinLvActiveOnLocalNode(thinPoolLv *v1alpha1.ThinPoolLv, name string) bool {