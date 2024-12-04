9a59ea315ec59cc156b9f4c6f1770c02434de1a7
Eric Blake <eblake@redhat.com>
2024-12-03 20:53:01 -0600

    tests: Broaden migrate.sh to work on Linear volumes
    Technically, we don't NEED migration when using Linear volumes - but
    we _can_ prove that the LV is activated on the first node prior to the
    attempt at migration, and that it is active on the second node after
    the first node is no longer using the PVC.  The test only needs a
    minor tweak to look for a witness that will be present in both Linear
    and (eventual) Thin mode setups.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>

--- tests/t/migrate.sh
+++ tests/t/migrate.sh
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
-ksan-supported-modes # TODO add Thin when RWX is implemented
+ksan-supported-modes Linear # TODO add Thin when RWX is implemented
 
 # SOME DEFINITIONS
 
@@ -74,12 +74,15 @@ ensure_pod_is_writing 1
 
 ksan-stage 'Ensuring that the first pod is still writing to the volume...'
 ensure_pod_is_writing 0
+# CAUTION: this code is fragile - it assumes knowledge of KubeSAN internals.
+# This dm device will only exist if the LV is active on the node.
+ksan-poll 1 300 "kubectl exec test-pod-0 -- dmsetup status | grep -q 'kubesan--vg-pvc--.*:'"
 
 ksan-stage 'Deleting the first pod...'
 kubectl delete pod test-pod-0 --timeout=30s
 
 ksan-stage 'Waiting until the blob pool has migrated...'
-ksan-poll 1 300 "kubectl exec test-pod-1 -- dmsetup status | grep -q 'kubesan--vg-pvc--.*--thin:'"
+ksan-poll 1 300 "kubectl exec test-pod-1 -- dmsetup status | grep -q 'kubesan--vg-pvc--.*:'"
 
 ksan-stage 'Ensuring that the second pod is still writing to the volume...'
 ensure_pod_is_writing 1
