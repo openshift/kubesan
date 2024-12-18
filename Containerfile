openshift/kubesan

commit 8fddf3f73e48fdbb2d9e4819ae9c832aaf3414ef

containerfile best-practices updates
We are updating a few things in the containerfile:
- Adding in TARGETOS TARGETARCH and TARGETPLATFORM args
  - These allow us to cross-compile and do multiarch builds
- Switched the builder image name from 'makefile' to the more standard 'builder'
- Switched to more generic base images which allows users to pull from
  their preferred repository
- Added the GOOS and GOARCH environment variables to the go build
- Added the platform flag to the fedora base image for multiarch builds
- Added dnf update to make sure all security patches are in the base image
- Added --nodocs and --noplugins for a slimmed down dnf install

Signed-off-by: Jeff Roche <jeroche@redhat.com>
[eblake: also fix a stale comment, hoist vendoring change to earlier patch,
drop attempt to use non-root user for a later patch]
Signed-off-by: Eric Blake <eblake@redhat.com>

Containerfile
@@ -1,8 +1,8 @@
 # SPDX-License-Identifier: Apache-2.0
-
-FROM quay.io/projectquay/golang:1.22 AS makefile
-
-RUN dnf install -qy diffutils && dnf clean all
+ARG TARGETOS
+ARG TARGETARCH
+ARG TARGETPLATFORM
+FROM golang:1.22 AS builder
 
 WORKDIR /kubesan
 
@@ -28,18 +28,24 @@ COPY deploy/ deploy/
 COPY hack/ hack/
 COPY internal/ internal/
 
-RUN make build
+# We set GOOS and GOARCH so go cross-compiles to the correct os and arch
+RUN GOOS=$TARGETOS GOARCH=$TARGETARCH make build
 
 # CentOS Stream 9 doesn't provide package nbd
 # FROM quay.io/centos/centos:stream9
-FROM quay.io/fedora/fedora:40
+# We use --platform=$TARGETPLATFORM to pull the correct arch for
+# the base image. This is needed for multi-arch builds
+FROM --platform=$TARGETPLATFORM fedora:40
 
 # util-linux-core, e2fsprogs, and xfsprogs are for Filesystem volume support where
 # blkid(8) and mkfs are required by k8s.io/mount-utils.
-RUN dnf install -qy nbd qemu-img util-linux-core e2fsprogs xfsprogs && dnf clean all
+RUN dnf update -y && dnf install --nodocs --noplugins -qy nbd qemu-img util-linux-core e2fsprogs xfsprogs && dnf clean all
 
 WORKDIR /kubesan
 
-COPY --from=makefile /kubesan/bin/kubesan bin/
+COPY --from=builder /kubesan/bin/kubesan bin/
+
+# TODO Should we use a nonroot user?
+# USER 65532:65532
 
 ENTRYPOINT [ "/kubesan/bin/kubesan" ]

commit 83bcd12b10a5ff2efbcf2a05473852f141cc55c9

maint: Prefer fully-qualified container names
Jeff intentionally switched to short names to make it easier for
downstream builds to pick up containers from alternative locations.
But our CI does not like bare names [1], and the podman docs recommend
qualified names over short names in order to avoid supply chain
attacks.  We could reconfigure our CI environment to work with short
names, but for the short term, it is faster to revert Jeff's change
(Jeff is okay working around it downstream).

[1] https://ci.kronosnet.org/job/kubesan/job/kubesan/job/MR-113/2/pipeline-console/?selected-node=23

Fixes: 8fddf3f73e ("containerfile best-practices updates")
CC: Jeff Roche <jeroche@redhat.com>
Signed-off-by: Eric Blake <eblake@redhat.com>

Containerfile
@@ -2,7 +2,7 @@
 ARG TARGETOS
 ARG TARGETARCH
 ARG TARGETPLATFORM
-FROM golang:1.22 AS builder
+FROM quay.io/projectquay/golang:1.22 AS builder
 
 WORKDIR /kubesan
 
@@ -35,7 +35,7 @@ RUN GOOS=$TARGETOS GOARCH=$TARGETARCH make build
 # FROM quay.io/centos/centos:stream9
 # We use --platform=$TARGETPLATFORM to pull the correct arch for
 # the base image. This is needed for multi-arch builds
-FROM --platform=$TARGETPLATFORM fedora:40
+FROM --platform=$TARGETPLATFORM quay.io/fedora/fedora:40
 
 # util-linux-core, e2fsprogs, and xfsprogs are for Filesystem volume support where
 # blkid(8) and mkfs are required by k8s.io/mount-utils.